a1d559697d292e4ede7f17b5c248c52ae3a47649
Mogens Beltoft <mogens@beltoft.dk>
2024-12-23 19:13:38 +0100

    Day23 work

--- Day23part2.cpp
+++ Day23part2.cpp
@@ -1,74 +1,96 @@
-#include <vector>
 #include <string>
+#include <vector>
+#include <map>
+#include <set>
 #include <fstream>
 #include <iostream>
-#include "StringSplit.h"
-
-std::vector<std::string> computers;
-std::vector<std::string> links;
-
-void addEdge(std::vector<std::vector<int>> &mat, const std::string &ca, const std::string &cb)
-{
-    size_t a = 0;
-    for (; a < computers.size(); a++) {
-        if (ca == computers[a]) {
-            break;
-        }
-    }
-    size_t b = 0;
-    for (; b < computers.size(); b++) {
-        if (cb == computers[b]) {
-            break;
-        }
-    }
-
-    mat[a][b] = 1;
-    mat[b][a] = 1; // Since the graph is undirected
-}
-
-void displayMatrix(std::vector<std::vector<int>> &mat)
-{
-    int V = mat.size();
-    for (int i = 0; i < V; i++) {
-        std::string comp = computers[i];
-        std::cout << comp << " ";
-        int sum = 0;
-        for (int j = 0; j < V; j++) {
-            std::cout << mat[i][j] << " ";
-            sum += mat[i][j];
-        }
-        std::cout << "= " << sum << std::endl;
-    }
-}
+#include <algorithm>
+#include <cstdint>
 
 void main()
 {
-    std::fstream file("input23test.txt");
-    std::string line;
-
-    // Create a graph with 4 vertices and no edges
-    // Note that all values are initialized as 0
-    int V = 16;
-    std::vector<std::vector<int>> mat(V, std::vector<int>(V, 0));
-    //std::vector<std::vector<int>> mat;
-
+    std::map<std::string,std::set<std::string>> adj;
+    std::set<std::string> computers;
+    std::string s;
+    
+    std::ifstream ifs("input23.txt");
     // kh-tc
-    while (std::getline(file, line)) {
-        auto list = StringSplit(line, '-');
-        for (const auto &computer : list) {
-            if (find(computers.begin(), computers.end(), computer) == computers.end()) {
-                computers.push_back(computer);
+    while (ifs >> s) {
+        std::string u = s.substr(0,2); // First computer
+        std::string v = s.substr(3,2); // Second computer
+        computers.insert(u);
+        computers.insert(v);
+        adj[u].insert(v); // These two computers are connected
+        adj[v].insert(u); // These two computers are connected
+    }
+    std::vector<std::string> v;
+    for (auto &i : computers) {
+        v.push_back(i);
+    }
+    std::cout << "Computers: " << v.size() << std::endl;
+    std::sort(v.begin(), v.end());
+    //find maximal clique
+    for(size_t i=0; i < v.size(); i++) {
+        for(size_t j = i+1; j < v.size(); j++) {
+            if (adj[v[i]].find(v[j]) == adj[v[i]].end()) {
+                continue;
+            }
+            for(size_t k = j+1; k < v.size(); k++) {
+                if (adj[v[i]].find(v[k]) == adj[v[i]].end() || adj[v[j]].find(v[k]) == adj[v[j]].end()) {
+                    continue;
+                }
+                for(size_t l = k+1; l < v.size(); l++) {
+                    if (adj[v[i]].find(v[l]) == adj[v[i]].end() || adj[v[j]].find(v[l]) == adj[v[j]].end() || adj[v[k]].find(v[l]) == adj[v[k]].end()) {
+                        continue;
+                    }
+                    for(size_t m = l+1; m < v.size(); m++) {
+                        if (adj[v[i]].find(v[m]) == adj[v[i]].end() || adj[v[j]].find(v[m]) == adj[v[j]].end() || adj[v[k]].find(v[m]) == adj[v[k]].end() || adj[v[l]].find(v[m]) == adj[v[l]].end()) {
+                            continue;
+                        }
+                        for(size_t n = m+1; n < v.size(); n++) {
+                            if (adj[v[i]].find(v[n]) == adj[v[i]].end() || adj[v[j]].find(v[n]) == adj[v[j]].end() || adj[v[k]].find(v[n]) == adj[v[k]].end() || adj[v[l]].find(v[n]) == adj[v[l]].end() || adj[v[m]].find(v[n]) == adj[v[m]].end()) {
+                                continue;
+                            }
+                            for(size_t o = n+1; o < v.size(); o++) {
+                                if (adj[v[i]].find(v[o]) == adj[v[i]].end() || adj[v[j]].find(v[o]) == adj[v[j]].end() || adj[v[k]].find(v[o]) == adj[v[k]].end() || adj[v[l]].find(v[o]) == adj[v[l]].end() || adj[v[m]].find(v[o]) == adj[v[m]].end() || adj[v[n]].find(v[o]) == adj[v[n]].end()) {
+                                    continue;
+                                }
+                                for(size_t p = o+1; p < v.size(); p++) {
+                                    if (adj[v[i]].find(v[p]) == adj[v[i]].end() || adj[v[j]].find(v[p]) == adj[v[j]].end() || adj[v[k]].find(v[p]) == adj[v[k]].end() || adj[v[l]].find(v[p]) == adj[v[l]].end() || adj[v[m]].find(v[p]) == adj[v[m]].end() || adj[v[n]].find(v[p]) == adj[v[n]].end() || adj[v[o]].find(v[p]) == adj[v[o]].end()) {
+                                        continue;
+                                    }
+                                    for(size_t q = p+1; q < v.size(); q++) {
+                                        if (adj[v[i]].find(v[q]) == adj[v[i]].end() || adj[v[j]].find(v[q]) == adj[v[j]].end() || adj[v[k]].find(v[q]) == adj[v[k]].end() || adj[v[l]].find(v[q]) == adj[v[l]].end() || adj[v[m]].find(v[q]) == adj[v[m]].end() || adj[v[n]].find(v[q]) == adj[v[n]].end() || adj[v[o]].find(v[q]) == adj[v[o]].end() || adj[v[p]].find(v[q]) == adj[v[p]].end()) {
+                                            continue;
+                                        }
+                                        for(size_t r = q+1; r < v.size(); r++) {
+                                            if (adj[v[i]].find(v[r]) == adj[v[i]].end() || adj[v[j]].find(v[r]) == adj[v[j]].end() || adj[v[k]].find(v[r]) == adj[v[k]].end() || adj[v[l]].find(v[r]) == adj[v[l]].end() || adj[v[m]].find(v[r]) == adj[v[m]].end() || adj[v[n]].find(v[r]) == adj[v[n]].end() || adj[v[o]].find(v[r]) == adj[v[o]].end() || adj[v[p]].find(v[r]) == adj[v[p]].end() || adj[v[q]].find(v[r]) == adj[v[q]].end()) {
+                                                continue;
+                                            }
+                                            for(size_t s = r+1; s < v.size(); s++) {
+                                                if (adj[v[i]].find(v[s]) == adj[v[i]].end() || adj[v[j]].find(v[s]) == adj[v[j]].end() || adj[v[k]].find(v[s]) == adj[v[k]].end() || adj[v[l]].find(v[s]) == adj[v[l]].end() || adj[v[m]].find(v[s]) == adj[v[m]].end() || adj[v[n]].find(v[s]) == adj[v[n]].end() || adj[v[o]].find(v[s]) == adj[v[o]].end() || adj[v[p]].find(v[s]) == adj[v[p]].end() || adj[v[q]].find(v[s]) == adj[v[q]].end() || adj[v[r]].find(v[s]) == adj[v[r]].end()) {
+                                                    continue;
+                                                }
+                                                for(size_t t = s+1; t < v.size(); t++) {
+                                                    if (adj[v[i]].find(v[t]) == adj[v[i]].end() || adj[v[j]].find(v[t]) == adj[v[j]].end() || adj[v[k]].find(v[t]) == adj[v[k]].end() || adj[v[l]].find(v[t]) == adj[v[l]].end() || adj[v[m]].find(v[t]) == adj[v[m]].end() || adj[v[n]].find(v[t]) == adj[v[n]].end() || adj[v[o]].find(v[t]) == adj[v[o]].end() || adj[v[p]].find(v[t]) == adj[v[p]].end() || adj[v[q]].find(v[t]) == adj[v[q]].end() || adj[v[r]].find(v[t]) == adj[v[r]].end() || adj[v[s]].find(v[t]) == adj[v[s]].end()) {
+                                                        continue;
+                                                    }
+                                                    for(size_t u = t+1; u < v.size(); u++) {
+                                                        if (adj[v[i]].find(v[u]) == adj[v[i]].end() || adj[v[j]].find(v[u]) == adj[v[j]].end() || adj[v[k]].find(v[u]) == adj[v[k]].end() || adj[v[l]].find(v[u]) == adj[v[l]].end() || adj[v[m]].find(v[u]) == adj[v[m]].end() || adj[v[n]].find(v[u]) == adj[v[n]].end() || adj[v[o]].find(v[u]) == adj[v[o]].end() || adj[v[p]].find(v[u]) == adj[v[p]].end() || adj[v[q]].find(v[u]) == adj[v[q]].end() || adj[v[r]].find(v[u]) == adj[v[r]].end() || adj[v[s]].find(v[u]) == adj[v[s]].end() || adj[v[t]].find(v[u]) == adj[v[t]].end()) {
+                                                            continue;
+                                                        }
+                                                        std::cout << v[i] << "," << v[j] << "," << v[k] << "," << v[l] << "," << v[m] << "," << v[n] << "," << v[o] << "," << v[p] << "," << v[q] << "," << v[r] << "," << v[s] << "," << v[t] << "," << v[u] << std::endl;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-        addEdge(mat, list[0], list[1]);
-        // Forward link
-        links.push_back(line);
-        // Backward link
-        std::string link = list[1] + "-" + list[0];
-        links.push_back(link);
     }
-    std::cout << "Computers: " << computers.size() << std::endl;
-    std::cout << "Links: " << links.size() << std::endl;
-
-    displayMatrix(mat);
 }

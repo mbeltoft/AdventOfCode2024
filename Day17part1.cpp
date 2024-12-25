509dfdb1f55ed6fa48c40748512e1ffb855ba52b
Mogens Beltoft <mogens@beltoft.dk>
2024-12-25 22:25:12 +0100

    Day17 work

--- Day17part1.cpp
+++ Day17part1.cpp
@@ -152,6 +152,13 @@ void Test1()
 
 void Part1()
 {
+    // Input:
+    // Register A: 35200350
+    // Register B: 0
+    // Register C: 0
+    //
+    // Program: 2,4,1,2,7,5,4,7,1,3,5,5,0,3,3,0
+
     uint64_t regA = 35200350L;
     uint64_t regB = 0;
     uint64_t regC = 0;
